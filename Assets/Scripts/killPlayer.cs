bf7952ac6177462f2ada26b23d4efb294fd4cdb3
rhianhorne07 <rhianhorne1w@gmail.com>
2025-12-11 21:44:20 +0000

    Added player death when falling into void

---
+++ Assets/Scripts/killPlayer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class killPlayer : MonoBehaviour
+{
+    public GameObject character;
+    public Transform respawnPoint;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            character.transform.position = respawnPoint.position;
+        }
+    }
+}
