815da6698e563ac72c331154690cf1e4faab9062
rhianhorne07 <rhianhorne1w@gmail.com>
2025-12-12 20:02:18 +0000

    Redesigned Level and Added dash ability

--- Assets/Scripts/killPlayer.cs
+++ Assets/Scripts/killPlayer.cs
@@ -7,16 +7,7 @@ public class killPlayer : MonoBehaviour
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
