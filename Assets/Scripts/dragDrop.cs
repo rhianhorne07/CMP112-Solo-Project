eb71922d024d89f72e1a0e56d3844002283c7dd5
rhianhorne07 <rhianhorne1w@gmail.com>
2025-12-13 16:56:30 +0000

    adding the puzzle pieces and attempting collision
    adding the puzzle pieces and attempting collision

--- Assets/Scripts/dragDrop.cs
+++ Assets/Scripts/dragDrop.cs
@@ -1,29 +1,61 @@
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class dragDrop : MonoBehaviour
 {
     private Vector3 dragOffset;
     private Camera camera;
+    public GameObject piece;
+    public GameObject collisionGrid;
+    private bool mouseDown = false;
+    private BoxCollider2D collider;
 
     private void Awake()
     {
         camera = Camera.main;
+        collider = GetComponent<BoxCollider2D>();
     }
-
-    private void OnMouseDown()
-    {
-        dragOffset = transform.position - getMousePos();
-    }
-
-    private void OnMouseDrag()
-    {
-                transform.position = getMousePos() + dragOffset;
-    }
-
     private Vector3 getMousePos()
     {
         var pos = camera.ScreenToWorldPoint(Input.mousePosition);
         pos.z = 0;
         return pos;
     }
+
+    private void OnMouseDown()
+    {
+        dragOffset = transform.position - getMousePos();
+        mouseDown = true;
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = getMousePos() + dragOffset;
+        mouseDown = true;
+    }
+
+    private void OnMouseUp()
+    {
+        mouseDown = false;
+        Debug.Log("released");
+        Debug.Log(collider.gameObject.CompareTag("collisionGrid"));
+
+
+        if (collider.gameObject.CompareTag("collisionGrid") && !mouseDown)
+        {
+            piece.transform.position = collisionGrid.transform.position;
+            Debug.Log("should be snapping");
+
+        }
+    }
+
+    //private void OnCollisionEnter2D(Collision2D other)
+    //{
+    //    Debug.Log(other.gameObject.CompareTag("collisionGrid"));
+    //    Debug.Log("colliding");
+
+
+    //}
+
+
 }
