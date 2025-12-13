09f7e21c3f0811c9c635859e29070c2c7a4fac40
rhianhorne07 <rhianhorne1w@gmail.com>
2025-12-13 14:26:37 +0000

    spirals are picked up and counted and dropped off in box. Side of platforms no longer have friction.

---
+++ Assets/Scripts/pickupPuzzle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+
+public class pickupPuzzle : MonoBehaviour
+{
+
+    public GameObject puzzlePiece;
+    public GameObject dropOffBox;
+    dropOffPuzzle dropOffScript;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        dropOffScript = dropOffBox.GetComponent<dropOffPuzzle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            puzzlePiece.SetActive(false);
+            dropOffScript.pieceCounter += 1;
+        }
+    }
+}
